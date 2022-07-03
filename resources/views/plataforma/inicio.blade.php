08e1064537b0ae96ba2670120fcfb2ab819deb38
Luis Fuentealba <lfuentealba@ezsolutions.cl>
2022-07-03 18:18:15 -0400

    Fix mis trabajos inicio

--- resources/views/plataforma/inicio.blade.php
+++ resources/views/plataforma/inicio.blade.php
@@ -41,29 +41,33 @@
 @if (Auth::user()->rol == 'trabajador')
 <div class="container">
     <h4>Mis Trabajos</h4>
-    @if ($trabajos->trabajador()->isEmpty())
-    <div class="alert alert-warning">
-        <p>No hay trabajos registrados</p>
-    </div>
-    @else
-    @foreach ($trabajos as $trabajo)
+    @php
+            $contadorT = 0;
+        @endphp
+        @foreach ($trabajos as $trabajo)
+            @if ($trabajo->rut_trabajador == Auth::user()->rut)
 
-
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">{{$trabajo->titulo}}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">{{$trabajo->estado}}</h6>
-              <p class="card-text">{{$trabajo->descripcion}}.</p>
-              <a href=href="{{ route('plataforma.trabajos.show', [
-                'trabajo' => $trabajo->id]) }} class="card-link">Ver detalles</a>
-              <a href="#" class="card-link">Finalizar</a>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">{{$trabajo->titulo}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{$trabajo->estado}}</h6>
+                <p class="card-text">{{$trabajo->descripcion}}.</p>
+                <a href=href="{{ route('plataforma.trabajos.show', [
+                    'trabajo' => $trabajo->id]) }} class="card-link">Ver detalles</a>
+                <a href="#" class="card-link">Finalizar</a>
+                </div>
             </div>
-          </div>
+            @php
+                $contadorT = $contadorT + 1;
+            @endphp
+            @endif
+        @endforeach
 
-
-
-    @endforeach
-    @endif
+        @if ($contadorT==0)
+            <div class="alert alert-warning">
+                <p>No hay trabajos registrados</p>
+            </div>
+        @endif
 </div>
 
 @endif
