d07580e892d7639876543ccc5498d7afec059ce8
Luis Fuentealba <lfuentealba@ezsolutions.cl>
2022-09-05 06:44:32 -0400

    Mejoras visuales OT

--- resources/views/plataforma/ot/export.blade.php
+++ resources/views/plataforma/ot/export.blade.php
@@ -8,130 +8,97 @@
     <title>Orden de trabajo</title>
 </head>
 
-<body>
+<body style="background-color: white;">
 
-    <div style="width: 100%">
-        <div style="width:30%; margin-left: 0px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; font-size: 20px; vertical-align: middle; color:darkturquoise;">
+    <div style="width: 100%; display:flex; vertical-align: top;">
+        <div
+            style="margin-left: 0px; font-family: Arial, Helvetica, sans-serif; font-size: 20px; vertical-align: middle; color:darkturquoise;">
             <img src="{{ asset('img/inicio/logo.png') }}" style="width: 100px; height: 50px;" alt="EZ">
         </div>
-        <div style="margin:0px auto ;">
+        <div style="margin:0px auto; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
             <h3>ORDEN DE TRABAJO</h3>
         </div>
-        <div style="border: 3px solid red; text-align: center; width:150px; height:50px; float: right;">
+        <div
+            style="border: 3px solid red; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; text-align: center; width:150px; height:55px; float: right; margin-right: 0;">
             <h5>Orden N°{{ $ot->id }} </h5>
         </div>
 
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col">
+    <br>
+    <br>
 
-            </div>
-            <div class="col">
+    <div style="border-collapse: collapse; display: flex; font-family: Arial, Helvetica, sans-serif;">
+        <div style="width: 50%; border: 1px solid white;">
+            <strong>Tipo requerimiento:</strong>
+            {{ $ot->tipo_requerimiento }}
+        </div>
+        <div style="width: 50%;border: 1px solid white;">
+            <strong>Fecha:</strong> @php
 
-            </div>
-            <div class="col">
-                <br>
-            </div>
+            echo \Carbon\Carbon::parse($ot->fecha)->format('d/m/Y');
+            @endphp
         </div>
     </div>
-    <br>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <strong>Tipo requerimiento:</strong>
-                {{ $ot->tipo_requerimiento }}
-            </div>
-            <div class="col">
 
-            </div>
-            <div class="col">
-                <strong>Fecha:</strong> @php
+    <br>
 
-                    echo \Carbon\Carbon::parse($ot->fecha)->format('d/m/Y');
-                @endphp
-            </div>
+    <div style="border-collapse: collapse; display: flex; font-family: Arial, Helvetica, sans-serif;">
+        <div style="width: 50%;border: 1px solid rgb(0, 145, 255);">
+            <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Nombre técnico: </strong></div>
+            {{ $ot->trabajador->nombres }} {{ $ot->trabajador->apellidos }}
+        </div>
+        <div style="width: 50%;border: 1px solid rgb(0, 145, 255);">
+            <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Nombre colaborador: </strong></div>
+            {{ $ot->nombre_colaborador }}
         </div>
     </div>
-    <br>
-    <div class="container">
-        <div class="row row-cols-2">
-            <div class="col">
-                <strong>Nombre técnico: </strong>
-                <br>
-                {{ $ot->trabajador->nombres }} {{ $ot->trabajador->apellidos }}
-            </div>
-            <div class="col">
-                <strong>Nombre colaborador: </strong>
-                <br>
-                {{ $ot->nombre_colaborador }}
-            </div>
+    <div style="border-collapse: collapse; display: flex; font-family: Arial, Helvetica, sans-serif;">
+        <div style="width: 50%;border: 1px solid rgb(0, 145, 255);">
+            <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Dirección: </strong></div>
+            {{ $ot->direccion }}
+        </div>
+        <div style="width: 50%;border: 1px solid rgb(0, 145, 255);">
+            <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Ciudad: </strong></div>
+            {{ $ot->ciudad }}
         </div>
     </div>
-    <br>
-    <div class="container">
-        <div class="row row-cols-2">
-            <div class="col">
-                <strong>Dirección: </strong>
-                <br>
-                {{ $ot->direccion }}
-            </div>
-            <div class="col">
-                <strong>Ciudad: </strong>
-                <br>
-                {{ $ot->ciudad }}
-            </div>
-        </div>
-    </div>
-    <br>
-    <div class="container">
-        <div class="row row-cols-2">
-            <div class="col">
-                <strong>Detalles equipo antiguo: </strong>
-                <br>
+    <div style="border-collapse: collapse; display: flex; font-family: Arial, Helvetica, sans-serif;">
+        <div style="width: 50%;border: 1px solid rgb(0, 145, 255);">
+            <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Detalles equipo antiguo: </strong></div>
                 {{ $ot->detalles_equipo_antiguo }}
             </div>
-            <div class="col">
-                <strong>Detalles equipo nuevo: </strong>
-                <br>
+            <div style="width: 50%;border: 1px solid rgb(0, 145, 255);">
+                <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Detalles equipo nuevo: </strong></div>
                 {{ $ot->detalles_equipo_nuevo }}
             </div>
         </div>
     </div>
-    <br>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <strong>Descripcion de la solución: </strong>
-                <br>
+
+<br>
+    <div style="border-collapse: collapse; display: flex; font-family: Arial, Helvetica, sans-serif; height: 200px;">
+        <div style="width: 100%; border: 2px solid rgb(0, 145, 255);">
+            <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Descripcion de la solución: </strong></div>
                 {{ $ot->descripcion_solucion }}
             </div>
         </div>
-    </div>
+
     <br>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <strong>Observaciones: </strong>
-                <br>
+
+    <div style="border-collapse: collapse; display: flex; font-family: Arial, Helvetica, sans-serif;height: 180px;">
+        <div style="width: 100%; border: 2px solid rgb(0, 145, 255);">
+            <div style="background-color: rgb(0, 145, 255); color: white;"><strong>Observaciones: </strong></div>
                 {{ $ot->observaciones }}
             </div>
         </div>
-    </div>
-    <br><br><br><br><br>
-    <div class="container align-middle">
-        <div class="row">
-            <div class="col">
 
-            </div>
-            <div class="col">
-                <img src="{{ asset($ot->firma . '.png') }}" alt="">
+<br>
+    <div style="display: flex; font-family: Arial, Helvetica, sans-serif;">
+            <div style="margin: 0px auto ;">
+                <img src="{{ asset($ot->firma . '.png') }}" alt="firma">
                 <br>
-                <strong>Firma colaborador </strong>
+                <strong style="border-top: 1px solid black;">Firma colaborador </strong>
             </div>
-            <div class="col">
 
-            </div>
         </div>
     </div>
 
