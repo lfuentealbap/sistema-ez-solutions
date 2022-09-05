939b6edbb8f9a0273eecd2ff27d28d4374d133c0
Luis Fuentealba <lfuentealba@ezsolutions.cl>
2022-09-05 05:13:27 -0400

    a

--- resources/views/plataforma/ot/export.blade.php
+++ resources/views/plataforma/ot/export.blade.php
@@ -9,134 +9,133 @@
 </head>
 
 <body>
-    <div style="background-color: white">
-        <div style="width: 100%">
-            <div
-                style="width:30%; margin-left: 0px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; font-size: 20px; vertical-align: middle; color:darkturquoise;">
-                <img src="{{ asset('img/inicio/logo.png') }}" style="width: 100px; height: 80px;"
-                    alt="EZ">
-            </div>
-            <div style="width:30%; margin:0px auto ;">
-                <h3>ORDEN DE TRABAJO</h3>
-            </div>
-            <div style="border: 3px solid red; text-align: center; width:30%; height:80px; margin-right:0px;">
-                <h5>Orden N°{{ $ot->id }} </h5>
-            </div>
 
+    <div style="width: 100%">
+        <div
+            style="width:30%; margin-left: 0px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; font-size: 20px; vertical-align: middle; color:darkturquoise;">
+            <img src="{{ asset('img/inicio/logo.png') }}" style="width: 100px; height: 80px;" alt="EZ">
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
+        <div style="width:30%; margin:0px auto ;">
+            <h3>ORDEN DE TRABAJO</h3>
+        </div>
+        <div style="border: 3px solid red; text-align: center; width:30%; height:50px; margin-right:0px;">
+            <h5>Orden N°{{ $ot->id }} </h5>
+        </div>
 
-                </div>
-                <div class="col">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
 
-                </div>
-                <div class="col">
-                    <br>
-                </div>
             </div>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <strong>Tipo requerimiento:</strong>
-                    {{ $ot->tipo_requerimiento }}
-                </div>
-                <div class="col">
+            <div class="col">
 
-                </div>
-                <div class="col">
-                    <strong>Fecha:</strong> @php
-
-                        echo \Carbon\Carbon::parse($ot->fecha)->format('d/m/Y');
-                    @endphp
-                </div>
             </div>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row row-cols-2">
-                <div class="col">
-                    <strong>Nombre técnico: </strong>
-                    <br>
-                    {{ $ot->trabajador->nombres }} {{ $ot->trabajador->apellidos }}
-                </div>
-                <div class="col">
-                    <strong>Nombre colaborador: </strong>
-                    <br>
-                    {{ $ot->nombre_colaborador }}
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row row-cols-2">
-                <div class="col">
-                    <strong>Dirección: </strong>
-                    <br>
-                    {{ $ot->direccion }}
-                </div>
-                <div class="col">
-                    <strong>Ciudad: </strong>
-                    <br>
-                    {{ $ot->ciudad }}
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row row-cols-2">
-                <div class="col">
-                    <strong>Detalles equipo antiguo: </strong>
-                    <br>
-                    {{ $ot->detalles_equipo_antiguo }}
-                </div>
-                <div class="col">
-                    <strong>Detalles equipo nuevo: </strong>
-                    <br>
-                    {{ $ot->detalles_equipo_nuevo }}
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <strong>Descripcion de la solución: </strong>
-                    <br>
-                    {{ $ot->descripcion_solucion }}
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <strong>Observaciones: </strong>
-                    <br>
-                    {{ $ot->observaciones }}
-                </div>
-            </div>
-        </div>
-        <br><br><br><br><br>
-        <div class="container align-middle">
-            <div class="row">
-                <div class="col">
-
-                </div>
-                <div class="col">
-                    <img src="{{ asset($ot->firma . '.png') }}" alt="">
-                    <br>
-                    <strong>Firma colaborador </strong>
-                </div>
-                <div class="col">
-
-                </div>
+            <div class="col">
+                <br>
             </div>
         </div>
     </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <strong>Tipo requerimiento:</strong>
+                {{ $ot->tipo_requerimiento }}
+            </div>
+            <div class="col">
+
+            </div>
+            <div class="col">
+                <strong>Fecha:</strong> @php
+
+                    echo \Carbon\Carbon::parse($ot->fecha)->format('d/m/Y');
+                @endphp
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row row-cols-2">
+            <div class="col">
+                <strong>Nombre técnico: </strong>
+                <br>
+                {{ $ot->trabajador->nombres }} {{ $ot->trabajador->apellidos }}
+            </div>
+            <div class="col">
+                <strong>Nombre colaborador: </strong>
+                <br>
+                {{ $ot->nombre_colaborador }}
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row row-cols-2">
+            <div class="col">
+                <strong>Dirección: </strong>
+                <br>
+                {{ $ot->direccion }}
+            </div>
+            <div class="col">
+                <strong>Ciudad: </strong>
+                <br>
+                {{ $ot->ciudad }}
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row row-cols-2">
+            <div class="col">
+                <strong>Detalles equipo antiguo: </strong>
+                <br>
+                {{ $ot->detalles_equipo_antiguo }}
+            </div>
+            <div class="col">
+                <strong>Detalles equipo nuevo: </strong>
+                <br>
+                {{ $ot->detalles_equipo_nuevo }}
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <strong>Descripcion de la solución: </strong>
+                <br>
+                {{ $ot->descripcion_solucion }}
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <strong>Observaciones: </strong>
+                <br>
+                {{ $ot->observaciones }}
+            </div>
+        </div>
+    </div>
+    <br><br><br><br><br>
+    <div class="container align-middle">
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col">
+                <img src="{{ asset($ot->firma . '.png') }}" alt="">
+                <br>
+                <strong>Firma colaborador </strong>
+            </div>
+            <div class="col">
+
+            </div>
+        </div>
+    </div>
+
 
 </body>
 
