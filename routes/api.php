981db078e7d469e3b6b8d8acd17867cffbb38f8a
Luis Fuentealba <luis.fuentealba1501@alumnos.ubiobio.cl>
2022-09-30 18:47:53 -0300

    Actualización links api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\api\ClienteController;
+use App\Http\Controllers\api\CotizacionController;
+use App\Http\Controllers\api\GastoController;
+use App\Http\Controllers\api\OTController;
+use App\Http\Controllers\api\ProductoController;
+use App\Http\Controllers\api\TrabajadorController;
+use App\Http\Controllers\api\TrabajoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +25,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('cotizaciones', CotizacionController::class);
+Route::apiResource('gastos', GastoController::class);
+Route::apiResource('ots', OTController::class);
+Route::apiResource('productos', ProductoController::class);
+Route::apiResource('trabajadores', TrabajadorController::class);
+Route::apiResource('trabajos', TrabajoController::class);
